ffd3d209a85565c69c9c317ca30775459cded698
Greg Magolan <gmagolan@gmail.com>
2018-04-03 10:30:43 -0700

    Add ngrx

--- src/hello-world/hello-world.module.ts
+++ src/hello-world/hello-world.module.ts
@@ -1,9 +1,12 @@
 import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
 
 import {HelloWorldComponent} from './hello-world.component';
 
 @NgModule({
   declarations: [HelloWorldComponent],
+  imports: [RouterModule],
   exports: [HelloWorldComponent],
 })
-export class HelloWorldModule {}
+export class HelloWorldModule {
+}
