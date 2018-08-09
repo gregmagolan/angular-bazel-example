8acae88bccf5a86bc349a2837de169b564ca611d
Greg Magolan <gmagolan@gmail.com>
2018-08-08 17:02:28 -0700

    Add comment

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+// `tslib` must be imported explicitly into the application
+// when building angular from source using bazel
 import 'tslib';
 
 import {platformBrowser} from '@angular/platform-browser';
