bded191801bc4f7c330d6522ba95b6d62e3c0f81
Greg Magolan <gmagolan@gmail.com>
2019-06-25 13:35:35 -0700

    WIP: example of using @auth0/angular-jwt npm package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {JwtModule} from '@auth0/angular-jwt';
 import {StoreModule} from '@ngrx/store';
 
 import {MaterialModule} from '../shared/material/material.module';
@@ -21,13 +22,19 @@ import {StorageModule} from './storage/storage.module';
 import {SupportModule} from './support/support.module';
 import {todoReducer} from './todos/reducers/reducers';
 
+export function tokenGetter() {
+  return 'token';
+}
+
+
+
 @NgModule({
   declarations: [AppComponent],
   imports: [
     AppRoutingModule, BrowserModule, BrowserAnimationsModule, MaterialModule, HomeModule,
     StoreModule.forRoot({todoReducer}), BillingModule, ComputeModule, DatastoreModule,
     FunctionsModule, LoggingModule, MonitoringModule, NetworkingModule, RegistryModule,
-    StorageModule, SupportModule
+    StorageModule, SupportModule, JwtModule.forRoot({config: {tokenGetter}})
   ],
   exports: [AppComponent],
   bootstrap: [AppComponent],
